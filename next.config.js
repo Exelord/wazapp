01a8377dd806d9035d87b96fc3097863b6a47b76
Maciej Kwaśniak <kmaciek17@gmail.com>
2020-05-17 17:59:23 +0200

    TM

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withPlugins = require('next-compose-plugins');
 
+const withTM = require('next-transpile-modules')(['wazapp']);
+
 const bundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
 const plugins = [
-  bundleAnalyzer
+  bundleAnalyzer,
+  withTM
 ];
 
 const config = {};
