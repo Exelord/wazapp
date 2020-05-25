e14f4d0a0111340d254fc49f39f353bf6cd0d3b9
Maciej Kwaśniak <kmaciek17@gmail.com>
2020-05-25 20:15:32 +0200

    Optimize example

--- packages/@wazapp/tracking/index.ts
+++ packages/@wazapp/tracking/index.ts
@@ -1,9 +1,10 @@
 import { observable, action as mobxAction } from "mobx"
-import { observer as tracker } from 'mobx-react';
+import { observer as tracker, Observer as Tracker } from 'mobx-react';
 
 export const action = mobxAction.bound
 
 export {
   tracker,
+  Tracker,
   observable as tracked
 }
