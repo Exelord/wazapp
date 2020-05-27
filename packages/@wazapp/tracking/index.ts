c1b28b5764f1800502d1748347adf954ac9a7c87
Maciej Kwaśniak <kmaciek17@gmail.com>
2020-05-27 17:39:36 +0200

    Fix tracking

--- packages/@wazapp/tracking/index.ts
+++ packages/@wazapp/tracking/index.ts
@@ -1,10 +1,9 @@
 import { observable, action as mobxAction } from "mobx"
-import { observer as tracker, Observer as Tracker } from 'mobx-react';
+import { observer as tracker } from 'mobx-react';
 
 export const action = mobxAction.bound
 
 export {
   tracker,
-  Tracker,
   observable as tracked
 }
