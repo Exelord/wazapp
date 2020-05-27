dff65b3e8f292cefcb8442af30cfce15f2124edb
Maciej Kwaśniak <kmaciek17@gmail.com>
2020-05-27 17:48:42 +0200

    Allow for custom container

--- packages/@wazapp/core/src/app.tsx
+++ packages/@wazapp/core/src/app.tsx
@@ -1,11 +1,11 @@
 import React, { ReactNode } from 'react';
-import { createContext, useState } from 'react';
+import { createContext, useMemo } from 'react';
 import { Container } from './container';
 
 export const ContainerContext = createContext({});
 
-const App = ({ children }: { children: ReactNode }) => {
-  const [appContainer] = useState(new Container());
+const App = ({ container, children }: { container?: Container, children: ReactNode }) => {
+  const appContainer = useMemo(() => container || new Container(), [container]);
 
   return (
     <ContainerContext.Provider value={appContainer}>
