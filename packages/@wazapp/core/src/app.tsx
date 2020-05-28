78632166299b640b3aca2015b9b313dd613b3624
Maciej Kwaśniak <kmaciek17@gmail.com>
2020-05-28 16:59:47 +0200

    Reset guid on app render

--- packages/@wazapp/core/src/app.tsx
+++ packages/@wazapp/core/src/app.tsx
@@ -1,11 +1,14 @@
 import React, { ReactNode } from 'react';
 import { createContext, useMemo } from 'react';
 import { Container } from './container';
+import { resetGuid } from '@wazapp/utils';
 
 export const ContainerContext = createContext({});
 
 const Wazapp = ({ container, children }: { container?: Container, children: ReactNode }) => {
   const appContainer = useMemo(() => container || new Container(), [container]);
+
+  useMemo(resetGuid, []);
 
   return (
     <ContainerContext.Provider value={appContainer}>
