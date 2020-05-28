9190f6b1a43657938f004eb1b675249bf6d519d0
Maciej Kwaśniak <kmaciek17@gmail.com>
2020-05-28 17:55:39 +0200

    Fix types

--- packages/@wazapp/helpers/src/each.ts
+++ packages/@wazapp/helpers/src/each.ts
@@ -1,7 +1,7 @@
-import { createElement, Fragment } from "react";
+import { createElement, Fragment, ReactNode } from "react";
 import { guidFor } from "@wazapp/utils";
 
-function each<T>(collection: T[] | null | undefined, onItem: (item: T, index: number) => any | any, onEmpty: any = null) {
+function each<T>(collection: T[] | null | undefined, onItem: (item: T, index: number) => any | any, onEmpty: any = null): ReactNode {
   if (collection && collection.length) {
     return collection.map((item: any, index: number) => {
       const children =  typeof onItem === 'function' ? onItem(item, index) : onItem;
