0ae489151bef9d3be36d4a392384e9ee06cde3d7
Maciej Kwaśniak <kmaciek17@gmail.com>
2020-05-28 10:24:41 +0200

    Add classNames helpers

--- packages/@wazapp/helpers/index.ts
+++ packages/@wazapp/helpers/index.ts
@@ -1,3 +1,4 @@
 export { default as each } from './src/each';
 export { default as when } from './src/when';
 export { default as yieldChildren } from './src/yield-children';
+export { default as classNames } from './src/class-names';
