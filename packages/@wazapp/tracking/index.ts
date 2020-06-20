70f3b82c8826fb479f02b11209581005848e0f22
Maciej Kwaśniak <kmaciek17@gmail.com>
2020-06-21 00:44:40 +0200

    Parts of mobx

--- packages/@wazapp/tracking/index.ts
+++ packages/@wazapp/tracking/index.ts
@@ -1,5 +1,55 @@
 import { action as mobxAction } from 'mobx';
 
-export * from "mobx";
-
 export const action = mobxAction.bound;
+
+export {
+  isArrayLike,
+  isObservable,
+  isBoxedObservable,
+  isObservableObject,
+  isObservableArray,
+  isObservableMap,
+  isObservableSet,
+  isObservableProp,
+  isComputedProp,
+  isComputed,
+  isAction,
+  observable,
+  ObservableMap,
+  ObservableSet,
+  computed,
+  
+  observe,
+  intercept,
+  autorun,
+  reaction,
+  when,
+  runInAction,
+  
+  keys,
+  values,
+  entries,
+  set,
+  remove,
+  has,
+  get,
+
+  toJS
+} from "mobx";
+
+export type {
+  IObservable,
+  IReactionPublic,
+  IReactionDisposer,
+  IAction,
+  IComputedValue,
+  IInterceptor,
+  IObservableObject,
+  IObservableValue,
+  IObservableArray,
+  IComputed,
+  IAutorunOptions,
+  IReactionOptions,
+  IWhenOptions,
+  IComputedValueOptions,
+} from "mobx";
