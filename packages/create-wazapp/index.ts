7351c214c882d290212de30274f17de2606eda27
Maciej Kwaśniak <kmaciek17@gmail.com>
2020-06-21 13:09:17 +0200

    Colors

--- packages/create-wazapp/index.ts
+++ packages/create-wazapp/index.ts
@@ -31,8 +31,16 @@ function createNextApp(name: string) {
   const repo = 'https://github.com/Wazappjs/wazapp';
   const path = 'blueprints/nextjs';
 
-  const ls = child_process.spawn('yarn', ['create', 'next-app', name, `-e`, repo, `--example-path`, path]);
-
-  ls.stdout.on('data', (data) => console.log(`${data}`));
-  ls.stderr.on('data', (data) => console.error(`${data}`));
+  child_process.spawn(
+    'yarn',
+    [
+      'create', 'next-app',
+      name,
+      `-e`, repo,
+      `--example-path`, path
+    ],
+    {
+      stdio: [process.stdin, process.stdout, process.stderr]
+    }
+  );
 }
