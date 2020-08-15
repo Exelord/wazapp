cc87b364231ec6e0cd4178c0a35b4ffafccdedb6
Maciej Kwaśniak <kmaciek17@gmail.com>
2020-08-15 10:09:31 +0200

    Fix types

--- packages/@wazapp/helpers/src/when.ts
+++ packages/@wazapp/helpers/src/when.ts
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
 
-export default function when<T>(a: T, b: T, c: T | null = null): ReactNode {
+export default function when<T>(a: any, b: T, c: any = null): ReactNode {
   return a ? b : c;
 }
