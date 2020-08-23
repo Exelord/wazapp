d8451c3fcb7469e182981a61adb17f70d24a9f56
Maciej Kwaśniak <kmaciek17@gmail.com>
2020-08-23 11:02:34 +0200

    Export classNames as cn

--- packages/@wazapp/helpers/index.ts
+++ packages/@wazapp/helpers/index.ts
@@ -1,4 +1,4 @@
 export { default as each } from './src/each';
 export { default as when } from './src/when';
 export { default as yieldChildren } from './src/yield-children';
-export { default as classNames } from './src/class-names';
+export { default as cn } from './src/class-names';
